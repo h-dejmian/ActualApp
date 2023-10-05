d2898d5b240457227516b19c379551b0301d2cc7
Michal Madej <mkmadej@interia.pl>
2023-10-05 16:48:23 +0200

    categories related fixes

--- src/main/java/com/example/ActualApp/controller/dto/NewActivityCategoryDto.java
+++ src/main/java/com/example/ActualApp/controller/dto/NewActivityCategoryDto.java
@@ -9,8 +9,5 @@ import java.util.List;
 public record NewActivityCategoryDto(
         @NotBlank
         String name
-//        @Range(min = 1, max = 7, message = "Priority should be number between 1 and 7")
-//        int priority,
-//        List<Activity> activities
 ) {
 }
