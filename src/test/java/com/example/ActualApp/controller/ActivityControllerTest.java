29000da3e0f4b6a5d559630a120ea92bf25b5f26
Michal Madej <mkmadej@interia.pl>
2023-12-08 17:32:38 +0100

    activity controller tests

--- src/test/java/com/example/ActualApp/controller/ActivityControllerTest.java
+++ src/test/java/com/example/ActualApp/controller/ActivityControllerTest.java
@@ -2,6 +2,7 @@ package com.example.ActualApp.controller;
 
 import com.example.ActualApp.controller.dto.ActivityDto;
 import com.example.ActualApp.controller.dto.NewActivityDto;
+import com.example.ActualApp.controller.dto.PlannedActivityDto;
 import com.example.ActualApp.service.ActivityService;
 import org.aspectj.lang.annotation.Before;
 import org.instancio.Instancio;
@@ -136,5 +137,99 @@ class ActivityControllerTest {
 
         //Then
         verify(activityService, times(1)).deleteActivity(activityId);
+    }
+
+    @Test
+    void shouldReturnActivitiesByDate() throws Exception {
+        //Given
+        List<ActivityDto> activities = Instancio.ofList(ActivityDto.class)
+                .size(1)
+                .create();
+        ActivityDto activityDto = activities.get(0);
+        String date = "2023-10-05";
+        UUID userId = UUID.randomUUID();
+
+        Mockito.when(activityService.getActivitiesByDate(date, userId)).thenReturn(activities);
+
+        //When
+        var response = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/activities?userId=" + userId + "&date=" + date));
+
+
+        //Then
+        response.andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(1))
+                .andExpect(jsonPath("$[0].id").value(activityDto.id().toString()))
+                .andExpect(jsonPath("$[0].description").value(activityDto.description()))
+                .andExpect(jsonPath("$[0].timeSpentInMinutes").value(activityDto.timeSpentInMinutes()))
+                .andExpect(jsonPath("$[0].date").value(activityDto.date().toString()))
+                .andExpect(jsonPath("$[0].completed").value(activityDto.completed()))
+                .andExpect(jsonPath("$[0].categoryName").value(activityDto.categoryName()));
+    }
+
+    @Test
+    void shouldReturnActivitiesWithTimeRange() throws Exception {
+        //Given
+        List<PlannedActivityDto> activities = Instancio.ofList(PlannedActivityDto.class)
+                .size(1)
+                .create();
+        PlannedActivityDto activityDto = activities.get(0);
+        String date = "2023-10-05";
+        UUID userId = UUID.randomUUID();
+
+        Mockito.when(activityService.getActivitiesWithTimeRangeByDate(LocalDate.parse(date), userId)).thenReturn(activities);
+
+        //When
+        var response = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/activities?planned&userId=" + userId + "&date=" + date));
+
+
+        //Then
+        response.andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(1))
+                .andExpect(jsonPath("$[0].id").value(activityDto.id().toString()))
+                .andExpect(jsonPath("$[0].description").value(activityDto.description()))
+                .andExpect(jsonPath("$[0].timeSpentInMinutes").value(activityDto.timeSpentInMinutes()))
+                .andExpect(jsonPath("$[0].date").value(activityDto.date().toString()))
+                .andExpect(jsonPath("$[0].completed").value(activityDto.completed()))
+                .andExpect(jsonPath("$[0].categoryName").value(activityDto.categoryName()))
+                .andExpect(jsonPath("$[0].startTime").value(activityDto.startTime().toString()))
+                .andExpect(jsonPath("$[0].endTime").value(activityDto.endTime().toString()));
+    }
+
+    @Test
+    void shouldUpdateActivity() throws Exception {
+        //Given
+        String id = "7d2381f7-9f3e-481e-8c0f-5e8e5498f32e";
+        UUID activityId = UUID.fromString("7d2381f7-9f3e-481e-8c0f-5e8e5498f32e");
+        ActivityDto activityDto = new ActivityDto(activityId, "Test description", 120,
+                LocalDate.of(2023, 10, 10), true, "Test Category", null, null);
+        String type = "regular";
+
+        Mockito.when(activityService.updateActivity(activityId, activityDto, type)).thenReturn(activityDto);
+
+        //When
+        var response = mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/activities/" + activityId + "?type=regular")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                                {                 
+                                        "id" : "7d2381f7-9f3e-481e-8c0f-5e8e5498f32e",     
+                                        "description" : "Test description",
+                                        "timeSpentInMinutes" : 120,
+                                        "date" : "2023-10-10",
+                                        "completed" : true,
+                                        "categoryName" : "Test Category",
+                                        "user_Id" : "fd70909e-fc1b-4313-95d7-d07da61d90d0",
+                                        "startTime" : null,
+                                        "endTime" : null
+                                }
+                                """));
+
+        //Then
+        response.andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(activityDto.id().toString()))
+                .andExpect(jsonPath("$.description").value(activityDto.description()))
+                .andExpect(jsonPath("$.timeSpentInMinutes").value(activityDto.timeSpentInMinutes()))
+                .andExpect(jsonPath("$.date").value(activityDto.date().toString()))
+                .andExpect(jsonPath("$.completed").value(activityDto.completed()))
+                .andExpect(jsonPath("$.categoryName").value(activityDto.categoryName()));
     }
 }
